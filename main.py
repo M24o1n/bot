c0e3537a2fa6eb58f7c7a87ef7470121ae3efd44
Mohammad Almuallimi <78993584+M24o1n@users.noreply.github.com>
2023-01-10 20:43:33 +0300

    hide token

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ import bot
 
 if __name__ == '__main__' :
     bot.run_bot()
-
-
-#invite link: https://discord.com/api/oauth2/authorize?client_id=1062356450229358612&permissions=1634235578432&scope=bot
-
-#Token: MTA2MjM1NjQ1MDIyOTM1ODYxMg.GjTWuc.nVmXrYt6Itk2Avck46gjDXiHdS1nL7g5evs0Oc
